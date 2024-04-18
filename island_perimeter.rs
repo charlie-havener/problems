92018934e452ca5eff60943a8f702089071f63a4
Charlie Havener <charles.havener@proton.me>
2024-04-17 19:35:47 -0700

    LC#463

---
+++ island_perimeter.rs
@@ -0,0 +1,48 @@
+pub fn island_perimeter(mut grid: Vec<Vec<i32>>) -> i32 {
+    let mut perimeter = 0;
+
+    // find a piece of land;
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if grid[row][col] == 1 {
+
+                // up
+                if row == 0 || grid[row-1][col] == 0 {
+                    perimeter += 1;
+                }
+
+                // down
+                if row == grid.len() - 1 || grid[row+1][col] == 0 {
+                    perimeter += 1;
+                }
+
+                // left 
+                if col == 0 || grid[row][col-1] == 0 {
+                    perimeter += 1;
+                }
+
+                // right
+                if col == grid[0].len() - 1 || grid[row][col+1] == 0 {
+                    perimeter += 1;
+                }
+            }
+        }
+    }
+
+    return perimeter;
+
+}
+
+#[test]
+fn tests() {
+    let grid = vec![vec![0,1,0,0],vec![1,1,1,0],vec![0,1,0,0],vec![1,1,0,0]];
+    assert_eq!(16, island_perimeter(grid));
+
+    let grid = vec![vec![1]];
+    assert_eq!(4, island_perimeter(grid));
+
+    let grid = vec![vec![1,0]];
+    assert_eq!(4, island_perimeter(grid));
+    
+}
+
