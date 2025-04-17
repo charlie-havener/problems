dcd9c0c960183f570ff14702f2fd556360fae5dc
Charlie Havener <charles.havener@proton.me>
2025-04-16 19:18:34 -0700

    LC#2176

---
+++ count_equal_and_divisible_pairs_in_an_array.rs
@@ -0,0 +1,13 @@
+pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
+
+    let mut ans = 0;
+    for i in 0..(nums.len() - 1) {
+        for j in (i+1)..nums.len() {
+            if nums[i] == nums[j] && (i * j) % k as usize == 0 {
+                ans += 1;
+            }
+        }
+    }
+
+    return ans;
+}
