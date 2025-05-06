315f23d164f2f39e96a62cfce64770d09c479232
Charlie Havener <charles.havener@proton.me>
2025-05-05 17:43:57 -0700

    LC#1920

---
+++ build_array_from_permutation.rs
@@ -0,0 +1,7 @@
+pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+    let mut ans = vec![0; nums.len()];
+    for i in 0..nums.len() {
+        ans[i] = nums[nums[i] as usize];
+    }
+    return ans;
+}
